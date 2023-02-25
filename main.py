10823cb026287e32e21421a02491b068f18d0531
anilkeshwani <anilkeshwani@hotmail.com>
2023-02-25 10:33:18 +0100

    Clean up main.py and update requirements

--- main.py
+++ main.py
@@ -3,19 +3,18 @@
 from __future__ import annotations
 
 import arxiv
-import notion
 from arxiv import SortCriterion, SortOrder
-from typer import run
+from typer import Option, run
 
 
-"""
 def main(
-    query: str,
-    id_list: list[str] = [],
+    query: str = "",
+    id_list: list[str] = Option([], help="Comma-separated list of arXiv IDs"),
     max_results: int = 10,
     sort_by: SortCriterion = "submittedDate",
     sort_order: SortOrder = "descending",
     coerce_to_list: bool = True,
+    verbose: bool = True,
 ):
     search = arxiv.Search(
         query=query,
@@ -27,24 +26,11 @@ def main(
     results = search.results()
     if coerce_to_list:
         results = list(results)
-    for result in results:
-        print(f"{result.title} - {result.published}", end="\n\n")
+    if verbose:
+        for result in results:
+            print(f"{result.title} - {result.published}", end="\n\n")
     return results
+
 
 if __name__ == "__main__":
     run(main)
-"""
-
-from notion.client import NotionClient
-
-
-# Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
-client = NotionClient(token_v2="secret_in3t0eYVXryTuLb1kcz3CDYIehu6gTuhvaP13coQpNZ")
-
-# Replace this URL with the URL of the page you want to edit
-page = client.get_block("https://www.notion.so/Test-da3cb9863df944efa9c5aaf753cccfce")
-
-print("The old title is:", page.title)
-
-# Note: You can use Markdown! We convert on-the-fly to Notion's internal formatted text data structure.
-page.title = "The title has now changed, and has *live-updated* in the browser!"
