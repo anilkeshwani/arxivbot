3f0319273ad91ac60504588941219dd647ea904d
anilkeshwani <anilkeshwani@hotmail.com>
2023-02-26 04:22:59 +0100

    Add test case examples to docstring

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ from notion_client import Client
 def canonicalise_arxiv(arxiv_like: str) -> str:
     """
     Convert arXiv IDs and URLs to canonical-ish form usable by search_arxiv.
+
+    Handles cases like:
+    - arXiv:1905.00001
+    - https://arxiv.org/abs/1905.00001
+    - https://arxiv.org/pdf/1905.00001.pdf
+    - https://arxiv.org/abs/1905.00001v1
+    - https://arxiv.org/pdf/1905.00001v1.pdf
+    - moz-extension://cf2764a6-e6bc-4c15-93f7-ff7531c62f1c/pdfviewer.html?target=https://arxiv.org/pdf/2204.03067.pdf
     """
     if arxiv_like.startswith("moz-extension"):
         arxiv_like = urlparse(arxiv_like).query.split("=")[-1]
