7b138b61d7d510f1c98ac2f4d1ab323809d74c16
anilkeshwani <anilkeshwani@hotmail.com>
2023-11-23 13:14:48 +0100

    default to always downloading the arXiv pdf with --no_pdf flag to get paper without PDF

--- arxivbot/obsidian_importer.py
+++ arxivbot/obsidian_importer.py
@@ -94,11 +94,11 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("id_list", type=str, nargs="+")
     parser.add_argument("--max_results", type=int, default=10**10)
-    parser.add_argument("--pdf", action="store_true")
+    parser.add_argument("--no_pdf", action="store_false", dest="download_pdf")
     args = parser.parse_args()
     args.id_list = [canonicalise_arxiv(arxiv_id) for arxiv_id in args.id_list]
     for arxiv_paper in arxiv.Search(id_list=args.id_list, max_results=args.max_results).results():
-        write_obsidian_paper(arxiv_paper, None, PAPERS_DIR, PDFS_DIR, args.pdf, False)
+        write_obsidian_paper(arxiv_paper, None, PAPERS_DIR, PDFS_DIR, args.download_pdf, False)
 
 
 if __name__ == "__main__":
