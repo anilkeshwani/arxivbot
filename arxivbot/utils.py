13eb36de5646b044ebad40dc41036a175b69bb8f
anilkeshwani <anilkeshwani@hotmail.com>
2025-07-17 17:44:26 +0200

    Simplify canonicalise_arxiv: match first arXiv-like subsring inc. version number with regex

--- arxivbot/utils.py
+++ arxivbot/utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from urllib.parse import urlparse
+import re
 
 
 # TODO is this best practice?
@@ -19,27 +19,13 @@ def inflect_day(day: int):
 
 
 def canonicalise_arxiv(arxiv_like: str) -> str:
-    """
-    Convert arXiv IDs and URLs to canonical-ish form usable by search_arxiv.
-
-    Handles cases like:
-    - arXiv:1905.00001
-    - https://arxiv.org/abs/1905.00001
-    - https://arxiv.org/pdf/1905.00001.pdf
-    - https://arxiv.org/abs/1905.00001v1
-    - https://arxiv.org/pdf/1905.00001v1.pdf
-    - moz-extension://cf2764a6-e6bc-4c15-93f7-ff7531c62f1c/pdfviewer.html?target=https://arxiv.org/pdf/2204.03067.pdf
-    """
-    if arxiv_like == "":
+    """Extracts and returns the first arXiv identifier-like string (inc. optional version) from a string."""
+    if not arxiv_like:
         raise ValueError("Got empty string for arxiv_like.")
-    if arxiv_like.startswith("moz-extension"):
-        arxiv_like = urlparse(arxiv_like).query.split("=")[-1]
-    if arxiv_like.startswith("arXiv:"):
-        arxiv_like = arxiv_like[6:]
-    if arxiv_like.startswith("http"):
-        urlparse_result = urlparse(arxiv_like)
-        if urlparse_result.hostname is None or "arxiv.org" not in urlparse_result.hostname:
-            raise ValueError("URL does not point to arXiv")
-        return urlparse_result.path.strip("/").split("/")[-1].strip(".pdf")
+
+    # Regex for modern arXiv IDs, with optional version
+    match = re.search(r"(\d{4}\.\d{5}(v\d+)?)", arxiv_like)
+    if match:
+        return match.group(1)
     else:
-        return arxiv_like.strip()
+        raise ValueError("Could not find a valid arXiv identifier.")
